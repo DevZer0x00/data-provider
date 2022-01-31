9c8c7e0e56f14c3da15fb91ed17dc1fd51071fa5
DevZer0x00 <dev.zer0x00@gmail.com>
2022-01-31 12:59:28 +0300

    Mysql container wait

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 composer install -n
+
+i=0
+until [ $i -ge 20 ]
+  do
+    nc -z dataprovider_mysql 3306 2>/dev/null && break
+    i=$(( i + 1 ))
+    echo "$i: Waiting for mysql .."
+    sleep 5
+done
+
+if [ $i -eq 20 ]; then
+  echo "Mysql unavailable, terminating ..."
+  exit 1
+fi
+
 php vendor/bin/codecept run
